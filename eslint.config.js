fe5f68f7b61db4088ebdbd8dfedb75bf7db166eb
Joseph Wang <jjosephwwang@gmail.com>
2024-11-22 20:13:16 -0800

    Integrate prettier with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,40 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-    js.configs.recommended,
-    {
-        rules: {
-            "no-unused-vars": "warn"
-        }
-    }
+  js.configs.recommended,
+  {
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
+      ...jsxA11yPlugin.configs.recommended.rules,
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  prettierConfig,
 ];
