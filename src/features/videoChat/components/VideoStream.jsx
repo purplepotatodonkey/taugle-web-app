6e56a07ce73fd9d877bff41f6671b99fb5fd1987
Bobby Dim <bobbydim@Bobbys-MBP-3.carolina.rr.com>
2024-11-27 21:49:54 -0500

    Add remoteStream, PC, and Socket to VideoStreamProvider

--- src/features/videoChat/components/VideoStream.jsx
+++ src/features/videoChat/components/VideoStream.jsx
@@ -2,16 +2,24 @@ import React, { useContext, useEffect, useRef } from 'react';
 import { VideoStreamContext } from '../providers/VideoStreamProvider';
 
 const VideoStream = () => {
-  const { localStream, initLocalStream } = useContext(VideoStreamContext);
-  const userVideoRef = useRef(null);
+  const { localStream, initLocalStream, remoteStream, initRemoteStream } = useContext(VideoStreamContext);
+  const userVideoRef = useRef(undefined);
+  const remoteVideoRef = useRef(undefined);
 
   useEffect(() => {
     initLocalStream(userVideoRef);
+    initRemoteStream(remoteVideoRef);
   }, []);
 
   return (
     <div className="h-full bg-gray-400 rounded-2xl">
       <div className="relative w-full h-full">
+        <video 
+          className='w-full h-full object-cover'
+          ref={remoteVideoRef}
+          autoPlay
+          playsInline
+        />
         <div className="absolute top-0 right-0 bg-gray-500 rounded-bl-2xl rounded-tr-2xl w-[20%] h-[20%] overflow-hidden">
           <video
             className="w-full h-full object-cover"
--- src/features/videoChat/providers/VideoStreamProvider.jsx
+++ src/features/videoChat/providers/VideoStreamProvider.jsx
@@ -3,7 +3,46 @@ import React, { createContext, useRef } from 'react';
 export const VideoStreamContext = createContext();
 
 const VideoStreamProvider = ({ children }) => {
-  const localStream = useRef(null);
+  const socket = useRef(undefined);
+  const localStream = useRef(undefined);
+  const remoteStream = useRef(undefined);
+  const peerConnection = useRef(undefined);
+
+  async function initSocket() {
+    const socketInstance = io(process.env.SOCKET_SERVER_URL)
+
+      socketInstance.on('connect', () => {
+        console.log('Connected to Socket.IO server');
+      });
+
+      socketInstance.on('disconnect', () => {
+        console.log('Disconnected from Socket.IO server');
+      });
+
+      socketInstance.on('connect_error', (error) => {
+        console.error('Connection error:', error);
+      });
+
+      socketInstance.on('offer', async (offer) => {
+        console.log('Offer received log:', offer);
+        await createAnswer(offer)
+      });
+
+      socketInstance.on('answer', async (answer) => {
+        console.log('Answer received log:', answer);
+        await setAnswer(answer); // Automatically set the answer when received
+      });
+
+      socketInstance.on('ice-candidate', async (candidate) => {
+        console.log('Received ice-candidate log:', candidate);
+        if (peerConnection.current) {
+          peerConnection.current.addIceCandidate(new RTCIceCandidate(candidate));
+        }
+      });
+      console.log("socketInstance in initSocket:", socketInstance)
+      socket.current = socketInstance;
+      console.log("socket.current in initSocket:", socket.current)
+    }
 
   async function initLocalStream(videoRef) {
     try {
@@ -26,8 +65,48 @@ const VideoStreamProvider = ({ children }) => {
     }
   }  
 
+  async function initRemoteStream(VideoRef) {
+    remoteStream.current = new MediaStream();
+    console.log("Remote stream is on")
+
+    if (VideoRef) {
+      VideoRef.srcObject = remoteStream.current
+    }
+  }
+
+  async function initPeerConnection() {
+    if (!localStream.current || !remoteStream.current) {
+      console.error("Cannot initialize peer connection: localStream or remoteStream is not set.");
+      return;
+    }
+
+    peerConnection.current = new RTCPeerConnection(configuration)
+    console.log("Peer connection has been created.")
+  
+    localStream.current.getTracks().forEach((track) => peerConnection.current.addTrack(track, localStream.current));
+  
+    peerConnection.current.ontrack = (e) => {
+      e.streams[0].getTracks().forEach((track) => remoteStream.current.addTrack(track));
+    };
+  
+    peerConnection.current.onicecandidate = (e) => {
+      if (e.candidate) {
+        socket.current.emit('ice-candidate', e.candidate);
+      }
+    };
+  }
+
   return (
-    <VideoStreamContext.Provider value={{ localStream, initLocalStream }}>
+    <VideoStreamContext.Provider value={{
+      initSocket,
+      socket,
+      localStream,
+      initLocalStream,
+      remoteStream,
+      initRemoteStream,
+      pc: peerConnection,
+      initPeerConnection,
+    }}>
       {children}
     </VideoStreamContext.Provider>
   );
